22fecfeabe03fec6f0be2c94b401d818d2727d2a
Romherre <98140472+Romherre@users.noreply.github.com>
2026-09-16 13:30:15 -0300

    Update Conexion.php

--- class/Conexion.php
+++ class/Conexion.php
@@ -2,16 +2,22 @@
 
 class Conexion
 {
-    public $DB_SERVER = "localhost";
-    public $DB_USER = "root";
-    public $DB_PASS = "";
-    public $DB_NAME = "accesibles";
+    public $DB_SERVER;
+    public $DB_USER;
+    public $DB_PASS;
+    public $DB_NAME;
     public $DB_DSN = null;
     public ?PDO $db = null;
 
     public function __construct()
     {
         try {
+            $config = include __DIR__ . '/../config.php';
+            $this->DB_SERVER = $config['db_server'];
+            $this->DB_USER   = $config['db_user'];
+            $this->DB_PASS   = $config['db_pass'];
+            $this->DB_NAME   = $config['db_name'];
+
             $this->DB_DSN = "mysql:host=" . $this->DB_SERVER . ";dbname=" . $this->DB_NAME . ";charset=utf8mb4";
             $this->db = new PDO($this->DB_DSN, $this->DB_USER, $this->DB_PASS, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         } catch (Exception $e) {
